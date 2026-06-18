6be079985753ebc6842ad8d2bd85d0a1c5698845
ZaidBelayachi <zaid.belayachii@gmail.com>
2026-06-18 19:23:16 +0100

    implemented 3D canvas model using React Three Fiber, fixed alignement issues on small mobile screens , fixed 3D model loading lag with a custom Loader component , added scroll-triggered fade-in animations using Framer Motion, added interactive hover tilt effect to project cards

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
 
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
@@ -11,13 +11,45 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <pointLight intensity={1} />
-      <primitive  object={computer.scene} />
+      <spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+      <primitive  object={computer.scene} scale={isMobile ? 0.7 : 0.75} position={isMobile ? [0, -3, -2.2] : [0 , -3.25, -1.5]} rotation={[-0.01, -0.2, -0.1]} />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
-  
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() =>{
+    //add an eventlistener for changes to the screen size
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    // set the initial value of 'isMobile' state variable
+    setIsMobile(mediaQuery.matches);
+    //define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) =>{
+      setIsMobile(event.matches);
+    }
+    //add the callback function as listener for changes to the media query
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    //remove the listener when the component is unmounted
+    return () =>{
+    mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+  }, [])
+
+  return (
+    <Canvas frameloop='demand'
+     shadows
+      camera={{ position : [20, 3, 5], fov: 25 }} gl={{preserveDrawingBuffer: true}}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls  enableZoom={false}  maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}/>
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all  />
+    </Canvas>
+  )
 }
 
-export default Computers
+export default ComputersCanvas;
